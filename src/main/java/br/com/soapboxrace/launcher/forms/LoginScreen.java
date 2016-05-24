75679d482e697e5f59537199acdc30f543c6bc99
berkay2578 <berkaytgy@gmail.com>
2016-05-24 21:50:37 +0300

    Add onSelection event handlers to the settings menu subitems

--- src/main/java/br/com/soapboxrace/launcher/forms/LoginScreen.java
+++ src/main/java/br/com/soapboxrace/launcher/forms/LoginScreen.java
@@ -114,15 +114,28 @@ public class LoginScreen extends Shell {
 		mntmServerSelect.setText("Select a server...");
 		
 		MenuItem mntmKeepServerCache = new MenuItem(menu_1, SWT.CHECK);
+		mntmKeepServerCache.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) { UserPreferences.KeepServerCache = mntmKeepServerCache.getSelection(); }
+		});
 		mntmKeepServerCache.setToolTipText("When server data is retrieved, keep them for later use\r\n(Note: this will cause old data to be shown unless manually refreshed in the launcher)\r\n(Note-2: disabling this will also delete your current saved server data cache)");
 		mntmKeepServerCache.setText("Keep server cache");
+		mntmKeepServerCache.setSelection(UserPreferences.KeepServerCache);
 		
 		MenuItem mntmAutoUpdateServers = new MenuItem(menu_1, SWT.CHECK);
+		mntmAutoUpdateServers.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) { UserPreferences.AutoUpdateServers = mntmAutoUpdateServers.getSelection(); }
+		});
 		mntmAutoUpdateServers.setToolTipText("Whether should the launcher auto-retrieve latest list of servers and also download their latest data\r\n(Note: do not check this if your PC or your internet is slow)");
 		mntmAutoUpdateServers.setText("Auto-Update servers on start");
 		mntmAutoUpdateServers.setSelection(UserPreferences.AutoUpdateServers);
 
 		MenuItem mntmAutoLogin = new MenuItem(menu_1, SWT.CHECK);
+		mntmAutoLogin.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) { UserPreferences.AutoLogin = mntmAutoLogin.getSelection(); }
+		});
 		mntmAutoLogin.setToolTipText("Whether should the launcher auto-log you in to the current server");
 		mntmAutoLogin.setText("Auto-Login on start");
 		mntmAutoLogin.setSelection(UserPreferences.AutoLogin);
